e003b0e95a5f8619592812d136c43d95e71989ba
Eason Li <supereason333@gmail.com>
2026-06-27 21:45:17 +1200

    Basic embassy project stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use panic_probe as _;
+
+use embassy_executor::Spawner;
+
+// HAL Imports
+use embassy_rp::gpio;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    info!("Start");
+    let _led_pin = gpio::Output::new(p.PIN_25, gpio::Level::High);
+    loop {}
 }
