fb000a0ca3388d60ae4bff048ab19a8d14d43d06
Eason Li <supereason333@gmail.com>
2026-07-24 16:57:48 +1200

    DAC using static buffers and implimenting sd static buffer transfer

--- src/dac.rs
+++ src/dac.rs
@@ -2,9 +2,11 @@ use defmt::*;
 
 use core::mem;
 
+use static_cell::StaticCell;
+
 use embassy_time::Instant;
 
-use embassy_futures::select::{Either, select};
+use embassy_futures::select::{Either, Either3, select, select3};
 
 use embassy_rp::peripherals::PIO0;
 use embassy_rp::pio_programs::i2s::{PioI2sOut, PioI2sOutProgram};
@@ -15,9 +17,9 @@ use embassy_sync::signal::Signal;
 use embedded_sdmmc::ShortFileName;
 
 use crate::dac;
-use crate::sd::{
-    AUDIO_CHUNK_BYTES, AUDIO_CHUNK_FRAMES, AUDIO_SD_REQUEST, AUDIO_SD_RESPONSE, AudioSdRequest,
-    AudioSdResponse, DirPath,
+use crate::sd::sd_task::{
+    AUDIO_CHUNK_BYTES, AUDIO_CHUNK_FRAMES, AUDIO_EMPTY, AUDIO_FILLED, AUDIO_SD_REQUEST,
+    AUDIO_SD_RESPONSE, AudioSdRequest, AudioSdResponse, DirPath,
 };
 
 pub static DAC_REQUEST: Signal<CriticalSectionRawMutex, DacRequest> = Signal::new();
@@ -32,9 +34,16 @@ pub enum DacRequest {
 pub const SAMPLE_RATE: u32 = 48_000;
 pub const BIT_DEPTH: u32 = 16;
 
+static I2S_FRONT: StaticCell<[u32; AUDIO_CHUNK_FRAMES]> = StaticCell::new();
+static I2S_BACK: StaticCell<[u32; AUDIO_CHUNK_FRAMES]> = StaticCell::new();
+
 #[embassy_executor::task]
 pub async fn dac_task(mut i2s: PioI2sOut<'static, PIO0, 0>) {
     info!("[DAC] DAC task spawned");
+    let front_buf: &'static mut [u32; AUDIO_CHUNK_FRAMES] =
+        I2S_FRONT.init([0u32; AUDIO_CHUNK_FRAMES]);
+    let back_buf: &'static mut [u32; AUDIO_CHUNK_FRAMES] =
+        I2S_BACK.init([0u32; AUDIO_CHUNK_FRAMES]);
     i2s.start();
     loop {
         loop {
@@ -61,41 +70,33 @@ pub async fn dac_task(mut i2s: PioI2sOut<'static, PIO0, 0>) {
             }
         }
 
-        // Start playback
+        // Start playback loop
 
         info!("[DAC] Recieved DAC request");
         AUDIO_SD_REQUEST.send(AudioSdRequest::ReadChunk).await;
-        let mut front_buf: [u32; AUDIO_CHUNK_FRAMES] = match AUDIO_SD_RESPONSE.wait().await {
-            AudioSdResponse::Chunk(pcm) => pcm_bytes_to_i2s_frames(&pcm),
-            _ => continue, // failed to get first chunk — bail back to outer loop
-        };
-        let mut back_buf: [u32; AUDIO_CHUNK_FRAMES] = [0u32; AUDIO_CHUNK_FRAMES];
+        let mut buf_a = AUDIO_FILLED.receive().await;
+        pcm_bytes_to_i2s_frames(buf_a, front_buf);
+        AUDIO_EMPTY.send(buf_a).await;
+
         AUDIO_SD_REQUEST.send(AudioSdRequest::ReadChunk).await;
         let mut i = 0;
         loop {
             // let start = Instant::now();
-            let future = i2s.write(&front_buf);
+            let future = i2s.write(front_buf);
             // let write = start.elapsed().as_millis();
 
-            match select(DAC_REQUEST.wait(), AUDIO_SD_RESPONSE.wait()).await {
-                Either::Second(sd_response) => match sd_response {
-                    AudioSdResponse::Chunk(pcm) => {
-                        AUDIO_SD_REQUEST.send(AudioSdRequest::ReadChunk).await;
-                        back_buf = pcm_bytes_to_i2s_frames(&pcm);
-                    }
-                    AudioSdResponse::Eof => {
-                        info!("[DAC] Playback ended");
-                        break;
-                    }
-                    AudioSdResponse::Error => {
-                        info!("[DAC] Playback error");
-                        break;
-                    }
-                    _ => {
-                        break;
-                    }
-                },
-                Either::First(dac_request) => match dac_request {
+            match select3(
+                AUDIO_FILLED.receive(),
+                DAC_REQUEST.wait(),
+                AUDIO_SD_RESPONSE.wait(),
+            )
+            .await
+            {
+                Either3::First(buf) => {
+                    pcm_bytes_to_i2s_frames(buf, back_buf);
+                    AUDIO_EMPTY.send(buf).await;
+                }
+                Either3::Second(dac_request) => match dac_request {
                     DacRequest::Start(path, name) => {
                         AUDIO_SD_REQUEST.send(AudioSdRequest::Close).await;
                         break;
@@ -108,11 +109,25 @@ pub async fn dac_task(mut i2s: PioI2sOut<'static, PIO0, 0>) {
                         break;
                     }
                 },
+                Either3::Third(sd_response) => match sd_response {
+                    AudioSdResponse::Eof => {
+                        info!("[DAC] Playback ended");
+                        break;
+                    }
+                    AudioSdResponse::Error => {
+                        info!("[DAC] Playback error");
+                        break;
+                    }
+                    _ => {
+                        break;
+                    }
+                },
+                _ => {}
             }
             // let response = start.elapsed().as_millis() - write;
             future.await;
             // let future_await = start.elapsed().as_millis() - response;
-            mem::swap(&mut back_buf, &mut front_buf);
+            mem::swap(back_buf, front_buf);
 
             // if i % 50 == 0 {
             //     let target = info!(
@@ -121,6 +136,10 @@ pub async fn dac_task(mut i2s: PioI2sOut<'static, PIO0, 0>) {
             //     );
             // }
             // i += 1;
+        }
+        // Cleanup and stuff (send buffers back)
+        while let Ok(buf) = AUDIO_FILLED.try_receive() {
+            AUDIO_EMPTY.send(buf).await;
         }
     }
 }
@@ -142,12 +161,13 @@ pub async fn dac_task(mut i2s: PioI2sOut<'static, PIO0, 0>) {
 // }
 
 // Single channel audio
-fn pcm_bytes_to_i2s_frames(bytes: &[u8; AUDIO_CHUNK_BYTES]) -> [u32; AUDIO_CHUNK_FRAMES] {
-    let mut frames = [0u32; AUDIO_CHUNK_FRAMES];
+fn pcm_bytes_to_i2s_frames(
+    bytes: &[u8; AUDIO_CHUNK_BYTES],
+    frames: &mut [u32; AUDIO_CHUNK_FRAMES],
+) {
     for (i, chunk) in bytes.chunks_exact(2).enumerate() {
         // 2 bytes per mono sample now, not 4
         let sample = i16::from_le_bytes([chunk[0], chunk[1]]) as u16;
         frames[i] = (sample as u32) | ((sample as u32) << 16); // duplicate into both L and R
     }
-    frames
 }
